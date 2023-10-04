0500c975433df3c9ba41a65197384e7824b9065b
BialyOrzel <adiwloadrczyk2004@gmail.com>
2023-10-04 14:43:57 +0200

    cos8

---
+++ 04_10_2023/Zad8/Zad8.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <title>Zad8</title>
+</head>
+<body>
+    <form method = "POST" action="Zad8_2.php">
+        Podaj liczbę 1 <br>
+        <input type="number"name="liczba1"><br>
+        Podaj liczbę 2 <br>
+        <input type="number"name="liczba2"><br>
+        Podaj liczbę 3 <br>
+        <input type="number"name="liczba3"><br>
+        <input type="submit" value="Wyślij">
+    </form>
+</body>
+</html>
---
+++ 04_10_2023/Zad8/Zad8_2.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <title>Zad8_2</title>
+</head>
+<body>
+    <?php
+        $liczba1 = $_POST["liczba1"];
+        $liczba2 = $_POST["liczba2"];
+        $liczba3 = $_POST["liczba3"];
+        if($liczba1 == $liczba2 && $liczba1 == $liczba3){
+            echo"Wszystkie liczby są równe ($liczba1)";
+        }elseif($liczba1 == $liczba2){
+            echo "Liczba 1 jest równa liczbie 2 ($liczba1 = $liczba2)";
+        }elseif($liczba1 == $liczba3){
+            echo "Liczba 1 jest równa liczbie 3 ($liczba1 = $liczba3)";
+        }elseif($liczba2 == $liczba3){
+            echo "Liczba 2 jest równa liczbie 3 ($liczba2 = $liczba3)";
+        }elseif($liczba3 == $liczba2){
+            echo "Liczba 3 jest równa liczbie 2 ($liczba3 = $liczba2)";
+        }else{
+            echo"Nie ma liczb równych ($liczba1), ($liczba2), ($liczba3)";
+        }
+    ?>
+</body>
+</html>
