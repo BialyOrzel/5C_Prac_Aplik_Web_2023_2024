7bc53e858226fe0254b7e84e6e4bb998553705af
BialyOrzel <adiwloadrczyk2004@gmail.com>
2023-10-05 13:57:24 +0200

    cos9

---
+++ 05_10_2023/Zad1.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <title>Zad1</title>
+</head>
+<body>
+    <form method = "POST" action="Zad8_2.php">
+        Podaj liczbę wierszy <br>
+        <input type="number"name="liczba1"><br>
+        Podaj liczbę kolumn <br>
+        <input type="number"name="liczba2"><br>
+        <input type="submit" value="Wyślij">
+    </form>
+</body>
+</html>
---
+++ 05_10_2023/Zad1_2.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <title>Zad1_2</title>
+</head>
+<body>
+    <?php
+        $liczba1 = $_POST["liczba1"];
+        $liczba2 = $_POST["liczba2"];
+        $liczba3 = $_POST["liczba3"];
+        while
+    ?>
+</body>
+</html>
