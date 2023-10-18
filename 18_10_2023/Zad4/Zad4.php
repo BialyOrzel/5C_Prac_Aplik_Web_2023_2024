90e6222fae247026f6ee03cbba3593cb3f3faa9d
BialyOrzel <adiwloadrczyk2004@gmail.com>
2023-10-18 14:25:09 +0200

    cos4

--- 18_10_2023/Zad4/Zad4.php
+++ 18_10_2023/Zad4/Zad4.php
@@ -6,8 +6,14 @@
 </head>
 <body>
     <form method = "POST" action="Zad4_2.php">
-        <label for="">Podaj tytuł filmu do us</label><br>
+        <label for="">Podaj ID które ma być edytowane</label><br>
+        <input type="number"name="ID"><br>
+        <label for="">Podaj nowy tytuł filmu  </label><br>
         <input type="text"name="tytul"><br>
+        <label for="">Podaj nowega reżysera</label><br>
+        <input type="text"name="rez"><br>
+        <label for="">Podaj nową długość filmu</label><br>
+        <input type="text"name="czas"><br>
         <input type="submit" value="Wyślij">
     </form>
 </body>
--- 18_10_2023/Zad4/Zad4_2.php
+++ 18_10_2023/Zad4/Zad4_2.php
@@ -5,15 +5,18 @@
     <title>Zad4_2</title>
 </head>
 <body>
-    <h1>Rekor w bazie danych został usunięty</h1>
+    <h1>Rekor w bazie danych został zmieniony</h1>
     <?php
         $host = "localhost";
         $user = "root";
         $pass = "";
         $db = "baza_filmow";
         $con = mysqli_connect($host, $user, $pass, $db);
+        $ID = $_POST["ID"];
         $tytul = $_POST["tytul"];
-        $pol = "DELETE FROM `filmy` WHERE tytul = '$tytul'";
+        $rez = $_POST["rez"];
+        $czas = $_POST["czas"];
+        $pol = "UPDATE `filmy` SET `tytul`='$tytul',`rezyser`='$rez',`czas`='$czas' WHERE ID = '$ID'";
         $wynik = mysqli_query($con, $pol);
     ?>
     </table>
