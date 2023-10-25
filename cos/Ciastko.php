47a9fab2ab5423e0bb21f7cc93e86585c3cbb48f
BialyOrzel <adiwloadrczyk2004@gmail.com>
2023-10-25 13:17:39 +0200

    cos2

---
+++ cos/Ciastko.php
@@ -0,0 +1,22 @@
+<?php
+    $cookie_name = "user";
+    $cookie_value = "Adrian Włodarczyk";
+    setcookie($cookie_name,$cookie_value, time()+ (60*60*24*30), "/");
+?>
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <title>Ciastko</title>
+</head>
+<body>
+    <?php
+        if(!isset($_COOKIE[$cookie_name])){
+            echo "Cookie name '". $cookie_name . "' is not set!";
+        }else{
+            echo "Cookie '". $cookie_name . "' is set!</br>";
+            echo "Value is: " . $_COOKIE[$cookie_name];
+        }
+    ?>
+</body>
+</html>
