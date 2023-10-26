fc37bb741154a06867449471b3654410dae4f09a
BialyOrzel <adiwloadrczyk2004@gmail.com>
2023-10-26 16:05:36 +0200

    egzamin1

---
+++ 26_10_2023/airport.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <title>Odloty samolotów</title>
+    <link rel ="stylesheet" href = "styl6.css">
+</head>
+<body>
+    <header>
+        <section id="b1">
+            <h2>Odloty z lotniska</h2>
+        </section>
+        <section id="b2">
+            <img src="zad6.png" alt="logotyp">
+        </section>
+    </header>
+    <main id ="m">
+        <h4>tabela odlotów</h4>
+        <table>
+            <tr>
+                <th>lp.</th>
+                <th>numer rejsu</th>
+                <th>czas</th>
+                <th>kierunek</th>
+                <th>status</th>
+            </tr>
+        </table>
+    </main>
+    <footer>
+        <section id = "f1">
+            <a href ="kw1.jpg" target = "_blank">Pobierz obraz</a>
+        </section>
+        <section id = "f2">
+            <?php
+            ?>
+        </section>
+        <section id = "f3">
+            Autor: Adrian Włodarczyk
+        </section>
+    </footer>
+</body>
+</html>
